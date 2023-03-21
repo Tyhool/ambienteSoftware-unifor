8222c880e2e8cb8bb8dd456b7a72cb5d690fe8d6
THYAGO PINHEIRO DINIZ <thy-diniz@hotmail.com>
2023-03-21 20:23:30 -0300

    Update teste.java

--- java/src/teste.java
+++ java/src/teste.java
@@ -2,5 +2,7 @@ public class teste {
     public void teste() {
         System.out.println("teste");
         System.out.println("site");
+        System.out.println("1234567890");
+        
     }
 }
