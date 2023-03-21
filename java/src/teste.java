0da5ff34586a268870792049b9146580d917c1e1
Thyago Diniz <thy-diniz@hotmail.com>
2023-03-21 20:27:18 -0300

    commit

--- java/src/teste.java
+++ java/src/teste.java
@@ -2,7 +2,6 @@ public class teste {
     public void teste() {
         System.out.println("teste");
         System.out.println("site");
-        System.out.println("1234567890");
-        
+
     }
 }
