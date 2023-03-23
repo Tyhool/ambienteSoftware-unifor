a2a8f3dc4482262c904601ec17cc5596a834bd4f
THYAGO PINHEIRO DINIZ <thy-diniz@hotmail.com>
2023-03-23 14:07:16 -0300

    Update teste.java

--- java/src/teste.java
+++ java/src/teste.java
@@ -2,5 +2,10 @@ public class teste {
     public void teste(){
         System.out.println("subindo");
         System.out.println("site-VSCode");
+        int a = 7;
+        int b = 2;
+        soma = 0;
+        soma = a + b;
+        System.out.println(soma);
     }
 }
