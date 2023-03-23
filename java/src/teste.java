8c27cab4b1345eec5c6e2172793937462a36a7d9
Thyago Diniz <thy-diniz@hotmail.com>
2023-03-23 14:07:44 -0300

    subindo

--- java/src/teste.java
+++ java/src/teste.java
@@ -1,10 +1,10 @@
 public class teste {
-    public void teste(){
+    public void teste() {
         System.out.println("subindo");
         System.out.println("site-VSCode");
         int a = 7;
         int b = 2;
-        soma = 0;
+        int soma = 0;
         soma = a + b;
         System.out.println(soma);
     }
